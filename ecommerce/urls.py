cc20b0fd98d27cd0db9aea1ab73820f80fd6bbba
hophoet <hophoet@gmail.com>
2020-07-24 14:25:44 +0000

    add of the media storage configs, path

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+#from django.config.urls import url 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('core.urls')),
+
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+
