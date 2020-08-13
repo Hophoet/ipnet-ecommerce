0be9a68e472d7de1e960650358d171742170fa75
adaboubvincent <adaboubvincent@gmail.com>
2020-08-13 23:44:08 +0200

    Modification du fichier urls.py de api

--- api/urls.py
+++ api/urls.py
@@ -5,9 +5,12 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
     path('categories/', views.listeCategories, name="categories"),
     path('categorie-enregistrer/', views.enregistrerCategorie, name="enregistrer_categorie"),
-
+    path('categorie/detail/<int:id>', views.detailCategorie, name="detail_categorie"),
 
     path('produits/', views.listeProduits, name="produits"),
     path('produit-enregistrer/', views.enregistrerProduit, name="enregistrer_produit"),
     path('produit/detail/<int:id>', views.detailProduit, name="detail_produit"),
+    
+    path('images/', views.listeImages, name="images"),
+    path('image/detail/<int:id>', views.detailImage, name="detail_image"),
 ]
