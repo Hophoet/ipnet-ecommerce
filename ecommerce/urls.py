11312d3a315606c835062e10b885bd83e62c4a7e
adaboubvincent <adaboubvincent@gmail.com>
2020-08-13 23:52:41 +0200

    Modification du fichier urls.py en ajoutant un path d'accueil

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -18,11 +18,17 @@ from django.conf.urls.static import static
 
 from django.contrib import admin
 from django.urls import path, include
+
+from . import views
+
 #from django.config.urls import url 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+
+
+    path('', views.accueil, name="accueil"),
 
 ]
 if settings.DEBUG:
