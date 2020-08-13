42d15cf0fbc72f90064368c58b4378adbebbd1a1
adaboubvincent <adaboubvincent@gmail.com>
2020-08-13 23:35:51 +0200

    Modification du fichier admin.py de api en  ajoutant les classes à Admin de django

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Produit
+from .models import Produit, Categorie, Fournisseur, Images
 # Register your models here.
 
 admin.site.register(Produit)
+admin.site.register(Fournisseur)
+admin.site.register(Categorie)
+admin.site.register(Images)
