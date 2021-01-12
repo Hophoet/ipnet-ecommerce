f55dae1d9e919c55ed3338ed515f4890c3b10d3b
hophoet <hophoet@gmail.com>
2021-01-12 01:25:30 +0000

    (models) - mise à jour du Fournisseur, categorie, produit, Image et create de Panier, ProuitACommander, Livraison, Commande

--- api/models.py
+++ api/models.py
@@ -1,12 +1,18 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
+
+#user model
+Utilisateur = settings.AUTH_USER_MODEL
 
 class Fournisseur(models.Model):
 	"""docstring for Fournisseur"""
 	nom = models.CharField(max_length=100, verbose_name="Nom du fournisseur ")
 	telephone = models.IntegerField(verbose_name="Téléphone du fournisseur ")
-
+	adresse = models.TextField()
+	email = models.EmailField()
+	estPerson = models.BooleanField(default=False)
 	class Meta:
 		"""docstring for Meta"""
 		unique_together = ['nom']
@@ -17,10 +23,8 @@ class Fournisseur(models.Model):
 
 class Categorie(models.Model):
 	"""docstring for Category"""
-	nom = models.CharField(max_length=100, verbose_name="Nom de la catégorie ")
-	description = models.TextField()
-	categorieParent = models.ForeignKey('self', on_delete = models.SET_NULL, null=True, blank=True, verbose_name="Catégorie parent ")
-	
+	nom = models.CharField(max_length=100, verbose_name="Nom de la catégorie")
+	description = models.TextField()	
 	class Meta:
 		"""docstring for Meta"""
 		unique_together = ['nom']
@@ -30,13 +34,12 @@ class Categorie(models.Model):
 			
 
 class Produit(models.Model):
-    nom = models.CharField(max_length=100, verbose_name="Nom du produit ")
+    nom = models.CharField(max_length=100, verbose_name="Nom du produit")
     prix = models.FloatField()
-    caracteristique = models.TextField(verbose_name="Caractéristique ")
-    quantite = models.IntegerField(verbose_name="Quantité ")
-    categorie = models.ForeignKey('Categorie', on_delete = models.CASCADE, verbose_name="Catégorie ")
-    fournisseur = models.ForeignKey('Fournisseur', on_delete = models.CASCADE, verbose_name="Fournisseur ")
-
+    caracteristique = models.TextField(verbose_name="Caractéristique")
+    quantite = models.IntegerField(verbose_name="Quantité")
+    categories = models.ManyToManyField(Categorie, related_name='produits')#models.ForeignKey('Categorie', on_delete = models.CASCADE, verbose_name="Catégorie ")
+    fournisseur = models.ForeignKey('Fournisseur', on_delete = models.CASCADE, verbose_name="Fournisseur")
 
     def __str__(self):
     	return "{}".format(self.nom)
@@ -44,12 +47,32 @@ class Produit(models.Model):
 def envoi_image_nom(instance, filename):
     title = instance.produit.nom
     slug = slugify(title)
-    return "post_images/%s-%s" % (slug, filename)
+    return "post_images/%s-%s" % (slug, filename) 
 
 
-class Images(models.Model):
+class Image(models.Model):
     produit = models.ForeignKey('Produit', on_delete = models.CASCADE, default=None)
-    image = models.ImageField(upload_to=envoi_image_nom, null=True, blank=True, verbose_name='Image')
+    url = models.ImageField(upload_to=envoi_image_nom, null=True, blank=True)
 
     def __str__(self):
     	return "{}".format(self.image)
+
+class Panier(models.Model):
+    utilisateur = models.ForeignKey(Utilisateur, on_delete=models.CASCADE)
+    estCommander = models.BooleanField(default=False)
+
+class ProduitACommander(models.Model):
+	produit = models.ForeignKey('Produit', on_delete=models.CASCADE)
+	panier = models.ForeignKey('Panier', on_delete=models.CASCADE)
+	quantite = models.IntegerField(verbose_name="Quantité")
+	estCommander = models.BooleanField(default=False)
+
+class Livraison(models.Model):
+    date = models.DateTimeField()
+    lieu = models.CharField(max_length=100)
+    estLivrer = models.BooleanField(default=False)
+
+class Commande(models.Model):
+	date = models.DateTimeField()
+	panier = models.OneToOneField('Panier', on_delete=models.SET_NULL, null=True, blank=True)
+	livraison = models.ForeignKey('Livraison', on_delete=models.CASCADE)
