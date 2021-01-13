1c8f9dffecbb0fcb803f034851701046c41b08e4
hophoet <hophoet@gmail.com>
2021-01-13 04:57:15 +0000

    (admin) - ajout de  Panier, ProduitACommander au site d'administration

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Produit, Categorie, Fournisseur, Image
+from .models import (Produit, Categorie, Fournisseur, Image,
+ProduitACommander, Panier)
 # Register your models here.
 
 admin.site.register(Produit)
 admin.site.register(Fournisseur)
 admin.site.register(Categorie)
 admin.site.register(Image)
+admin.site.register(Panier)
+admin.site.register(ProduitACommander)
