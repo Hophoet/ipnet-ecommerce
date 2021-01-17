e89397e22d6fd1f25cf2b518975bf7eac242c626
hophoet <hophoet@gmail.com>
2021-01-17 18:19:34 +0000

    (views) - ajout de view pour la recupération de tous les fournisseurs

--- api/views.py
+++ api/views.py
@@ -5,15 +5,27 @@ from rest_framework import status
 from rest_framework.views import APIView
 
 from .models import *
-from .serializers import CategorieSerializer, ProduitSerializer, ImagesSerializer
+from .serializers import CategorieSerializer, ProduitSerializer, ImagesSerializer, FournisseurSerializer
 
 
 class CategoriesListe(APIView):
     """ recupération de tous les categories disponible """
 
     def get(self, request, *args, **kwargs):
-        """ get methode requeste """
+        """ methode pour la requette get """
         # recuperation de toutes les categories
         categories = Categorie.objects.all()
         categories_serializer = CategorieSerializer(categories, many=True)
         return Response(categories_serializer.data)
+
+
+class FournisseursListe(APIView):
+    """ recupération de tous les categries disponible """
+
+    def get(self, request, *args, **kwargs):
+        """ methode pour la requette get """
+        # recuperation de tous les fournisseurs
+        fournisseurs = Fournisseur.objects.all()
+        fournisseurs_serializer = FournisseurSerializer(
+            fournisseurs, many=True)
+        return Response(fournisseurs_serializer.data, status=status.HTTP_200_OK)
