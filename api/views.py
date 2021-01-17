11cec25a1d2e506f9333b977bb37d2292500fd13
hophoet <hophoet@gmail.com>
2021-01-17 18:04:28 +0000

    (views) - ajout de view pour la recupération de toutes les categries disponibles

--- api/views.py
+++ api/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
-
+from rest_framework.views import APIView
 
 from .models import *
 from .serializers import CategorieSerializer, ProduitSerializer, ImagesSerializer
+
+
+class CategoriesListe(APIView):
+    """ recupération de tous les categories disponible """
+
+    def get(self, request, *args, **kwargs):
+        """ get methode requeste """
+        # recuperation de toutes les categories
+        categories = Categorie.objects.all()
+        categories_serializer = CategorieSerializer(categories, many=True)
+        return Response(categories_serializer.data)
