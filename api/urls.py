2562c12665e73c8bb61bb9f6cb0e4e8af1ca903d
hophoet <hophoet@gmail.com>
2021-01-17 19:16:00 +0000

    (urls) - ajout de la route de la view d'ajout au panier '

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('categories/', views.CategoriesListe().as_view(), name='categories'),
     path('fournisseurs/', views.FournisseursListe().as_view(), name='fournisseurs'),
     path('commandes/', views.UtilisateurCommandesListe().as_view(), name='commandes'),
+    path('ajout-au-panier/<int:produit_id>',
+         views.ajout_au_panier, name='ajout_au_panier')
 ]
