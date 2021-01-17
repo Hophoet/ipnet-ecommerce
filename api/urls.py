d7fbcc87e23a14588ba549e96dbd6a012c6c08db
hophoet <hophoet@gmail.com>
2021-01-17 18:39:16 +0000

    (urls) - ajout de la route de la view de recupération des commandes d'un utilisateur

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('categories/', views.CategoriesListe().as_view(), name='categories'),
     path('fournisseurs/', views.FournisseursListe().as_view(), name='fournisseurs'),
+    path('commandes/', views.UtilisateurCommandesListe().as_view(), name='commandes'),
 ]
