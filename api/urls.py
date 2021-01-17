ac00b847583bc79dea5688e6b797bac2fa093fb9
hophoet <hophoet@gmail.com>
2021-01-17 19:28:53 +0000

    (urls) - mise a jour du nom de la view compte tenue des modifications de la view

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('categories/', views.CategoriesListe().as_view(), name='categories'),
     path('fournisseurs/', views.FournisseursListe().as_view(), name='fournisseurs'),
     path('commandes/', views.UtilisateurCommandesListe().as_view(), name='commandes'),
-    path('ajout-au-panier/<int:produit_id>',
-         views.ajout_au_panier, name='ajout_au_panier')
+    path('ajout-au-panier/<int:produit_id>/',
+         views.AjoutAuPanierView().as_view(), name='ajout_au_panier')
 ]
